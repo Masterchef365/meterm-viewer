154098506ea294c8b661b1c6c3ca6a2f5258d36a
Masterchef365 <duncan.freeman1@gmail.com>
2024-06-20 16:32:58 -0700

    Get server from URL

--- src/app.rs
+++ src/app.rs
@@ -3,22 +3,22 @@ use metacontrols_client::ServerWidget;
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
 pub struct TemplateApp {
-    address: String,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            address: "ws://127.0.0.0:5000".to_string(),
         }
     }
 }
 
 impl TemplateApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        /*
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
+        */
 
         Default::default()
     }
@@ -26,17 +26,29 @@ impl TemplateApp {
 
 impl eframe::App for TemplateApp {
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+        //eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Connect to: ");
-                ui.text_edit_singleline(&mut self.address);
-            });
+        let server = "ws://127.0.0.1:5000".to_string();
 
-            ui.add(ServerWidget::new(&self.address));
+        // Get the server from the URL instead!
+        #[cfg(target_arch = "wasm32")]
+        let server: String = web_sys::window()
+            .unwrap()
+            .location()
+            .href()
+            .unwrap()
+            .split("?srv=")
+            .skip(1)
+            .next()
+            .map(|s| s.to_string())
+            .unwrap_or(server);
+
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label(&server);
+            ui.add(ServerWidget::new(&server));
         });
     }
 }
